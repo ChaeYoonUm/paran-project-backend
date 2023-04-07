1a632764bf795a39f5b9c6e03cc3f1f4f72d8261
ucy0303 <ucy0303@ajou.ac.kr>
2023-04-07 14:51:38 +0900

    Initial commit

---
+++ src/main/java/paran/paranprojectbackend/ParanProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package paran.paranprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParanProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParanProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/paran/paranprojectbackend/ParanProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package paran.paranprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParanProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
