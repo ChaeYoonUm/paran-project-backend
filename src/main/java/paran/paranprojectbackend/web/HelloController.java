ecc70b4c03a9374782bc93bfa10c4b5af265f202
ucy0303 <ucy0303@ajou.ac.kr>
2023-05-15 13:21:37 +0900

    add index page + /

--- src/main/java/paran/paranprojectbackend/web/HelloController.java
+++ src/main/java/paran/paranprojectbackend/web/HelloController.java
@@ -1,4 +1,3 @@
-/*
 package paran.paranprojectbackend.web;
 
 import org.springframework.stereotype.Controller;
@@ -11,4 +10,3 @@ public class HelloController {
         return "index";
     }
 }
-*/
