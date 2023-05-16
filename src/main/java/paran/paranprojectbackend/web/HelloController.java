690e4c865b592ce69266b4ce2b9b52771da6a2f7
ucy0303 <ucy0303@ajou.ac.kr>
2023-05-16 12:23:34 +0900

    edit hello controller_restController

--- src/main/java/paran/paranprojectbackend/web/HelloController.java
+++ src/main/java/paran/paranprojectbackend/web/HelloController.java
@@ -2,8 +2,9 @@ package paran.paranprojectbackend.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
     @GetMapping("/hello")
     public String index(){
