688d54781a236407240254caaeffe08fc6aea6fe
ucy0303 <ucy0303@ajou.ac.kr>
2023-05-16 13:50:42 +0900

    edit application.properties_add server info

--- src/main/java/paran/paranprojectbackend/web/HelloController.java
+++ src/main/java/paran/paranprojectbackend/web/HelloController.java
@@ -1,10 +1,12 @@
 package paran.paranprojectbackend.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RequiredArgsConstructor
+@Controller
 public class HelloController {
     @GetMapping("/hello")
     public String index(){
