ea67674b0f27739f63348ee5ecf642f83f90c716
ucy0303 <ucy0303@ajou.ac.kr>
2023-05-21 13:35:30 +0900

    add real clubpage

--- src/main/java/paran/paranprojectbackend/web/HelloController.java
+++ src/main/java/paran/paranprojectbackend/web/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/club")
     public String index(){
-        return "clubpage";
+        return "clublist";
     }
 }
